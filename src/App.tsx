637ef37cebe634c664c7e89c331cdf157d30e766
Deividwb <bernardinodeivid@gmail.com>
2023-03-13 16:55:49 -0300

    feat: formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Botao from "./components/Botao";
+import Formulario from "./components/Formulario";
 
-//teste
 function App() {
   return (
-    <div className="App" onClick={() => alert("deu certo")}>
-      <Botao />
-      <h1>OIIII</h1>
+    <div className="App">
+      <Formulario />
     </div>
   );
 }

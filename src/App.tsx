458b3eaff2bde950e98d2714212891ee46d8240d
Deividwb <bernardinodeivid@gmail.com>
2023-03-12 22:59:42 -0300

    feat: update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Botao from "./components/Botao";
 
 function App() {
-  return <div className="App">Start studies</div>;
+  return (
+    <div className="App" onClick={()=>alert("deu certo")}>
+      <Botao  />
+    </div>
+  );
 }
 
 export default App;

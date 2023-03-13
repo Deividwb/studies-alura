1e0b44881afc8fac37ce7ff8badd6bc3537fb15b
Deividwb <bernardinodeivid@gmail.com>
2023-03-13 08:26:25 -0300

    fix: test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Botao from "./components/Botao";
 
+//teste
 function App() {
   return (
-    <div className="App" onClick={()=>alert("deu certo")}>
-      <Botao  />
+    <div className="App" onClick={() => alert("deu certo")}>
+      <Botao />
     </div>
   );
 }

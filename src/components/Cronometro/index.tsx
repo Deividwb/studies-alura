11ac92d535df2f25d3b34c01edeee8d3b77f4838
Deividwb <bernardinodeivid@gmail.com>
2023-03-22 23:25:16 -0300

    fetch: cronometro e relogio

---
+++ src/components/Cronometro/index.tsx
@@ -0,0 +1,12 @@
+import Botao from "../Botao";
+
+export function Cronometro() {
+  return (
+    <div>
+      <p>Escolha um Card e inicie o Cronômetro</p>
+
+      <div></div>
+      <Botao texto={"Adicionar"} />
+    </div>
+  );
+}
